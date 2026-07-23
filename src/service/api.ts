be081e93619c0b491d7d602697f4ac9af03ca0cc
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-07-23 20:49:09 -0300

    merge:(merge) merge

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3000";
-// export const API_BASE_URL = "https://pede-gas-api.onrender.com";
+// export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://pede-gas-api.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
